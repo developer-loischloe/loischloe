875f1cd8e82fe8a4cdb3ec8e32c47e956dfcb11d
developer-loischloe <developer.loischloe@gmail.com>
2024-06-12 18:50:00 +0600

    countdown modified

--- src/components/Shared/countDown/OfferCountDown.tsx
+++ src/components/Shared/countDown/OfferCountDown.tsx
@@ -4,21 +4,35 @@ import CountDown from "./CountDown";
 
 const OfferCountDown = () => {
   const [timeInSideDhaka, setTimeInSideDhaka] = useState<null | Date>(
+    new Date("6/14/2024 20:00:00")
+  );
+  const [timeOutSideDhaka, setTimeOutSideDhaka] = useState<null | Date>(
     new Date("6/13/2024 20:00:00")
   );
 
   useEffect(() => {
-    setTimeInSideDhaka(new Date("6/13/2024 20:00:00"));
+    setTimeInSideDhaka(new Date("6/14/2024 20:00:00"));
+    setTimeOutSideDhaka(new Date("6/13/2024 20:00:00"));
   }, []);
 
   return (
     <section>
-      <h3 className="text-2xl md:text-4xl font-bold text-center mb-2 uppercase">
+      <h3 className="text-2xl md:text-4xl font-bold text-center mb-5 uppercase">
         Delivery Will Be Closed In :
       </h3>
-      <div className="flex justify-center gap-1">
-        <div>
-          <CountDown expiryTimestamp={timeInSideDhaka} />
+      <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
+        <div className="flex flex-col justify-center gap-1">
+          <h5 className="text-center font-bold text-lg">Inside Dhaka</h5>
+          <div>
+            <CountDown expiryTimestamp={timeInSideDhaka} />
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center gap-1">
+          <h5 className="text-center font-bold text-lg">Outside Dhaka</h5>
+          <div>
+            <CountDown expiryTimestamp={timeOutSideDhaka} />
+          </div>
         </div>
       </div>
     </section>
