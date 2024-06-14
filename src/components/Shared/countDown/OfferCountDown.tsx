1e70bc82232b730e2bb5b66831e058d31f26b0b7
developer-loischloe <162805095+developer-loischloe@users.noreply.github.com>
2024-06-14 22:03:23 +0600

    Update OfferCountDown.tsx

--- src/components/Shared/countDown/OfferCountDown.tsx
+++ src/components/Shared/countDown/OfferCountDown.tsx
@@ -6,13 +6,8 @@ const OfferCountDown = () => {
   const [timeInSideDhaka, setTimeInSideDhaka] = useState<null | Date>(
     new Date("6/18/2024 20:00:00")
   );
-/*  const [timeOutSideDhaka, setTimeOutSideDhaka] = useState<null | Date>(
-    new Date("6/13/2024 20:00:00")
-  );
-*/
-  useEffect(() => {
+ useEffect(() => {
     setTimeInSideDhaka(new Date("6/18/2024 20:00:00"));
-  //  setTimeOutSideDhaka(new Date("6/13/2024 20:00:00"));
   }, []);
 
   return (
@@ -22,20 +17,13 @@ const OfferCountDown = () => {
       </h3>
       <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
         <div className="flex flex-col justify-center gap-1">
-          <h5 className="text-center font-bold text-lg">Inside Dhaka</h5>
+         
           <div>
             <CountDown expiryTimestamp={timeInSideDhaka} />
           </div>
         </div>
 
-  /*
-        <div className="flex flex-col justify-center gap-1">
-          <h5 className="text-center font-bold text-lg">Outside Dhaka</h5>
-          <div>
-            <CountDown expiryTimestamp={timeOutSideDhaka} />
-          </div>
-        </div>
-  */
+
       </div>
       
     </section>
