8400fed168d4169cfd95acf200d3dc40e5c50179
developer-loischloe <162805095+developer-loischloe@users.noreply.github.com>
2024-06-20 09:48:15 +0600

    Update layout.tsx

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
     <>
       <GoogleTagManager gtmId={String(process.env.NEXT_PUBLIC_GTM_ID)} />
 
-      <LoisChloeMarquee />
+    //  <LoisChloeMarquee />
       <div className={`${belleza.variable} ${ooh_baby.variable}`}>
         {children}
       </div>
