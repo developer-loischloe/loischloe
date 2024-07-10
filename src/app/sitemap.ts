194bc9cbb82b5ec673feb384e6eb0c75b4ffc27f
developer-loischloe <developer.loischloe@gmail.com>
2024-07-10 17:52:06 +0600

    sitemap modified

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,41 @@
 import { MetadataRoute } from "next";
 import config from "@/config";
+import appwriteProductService from "@/appwrite/appwriteProductService";
+import appwriteBlogService from "@/appwrite/appwriteBlogService";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await appwriteProductService.getProductList({
+    p_category: "",
+    c_category: "",
+    n_category: "",
+    keyword: "",
+    page: "1",
+    resultPerPage: "1000",
+  });
+
+  const blogs = await appwriteBlogService.getAllBlog({
+    page: "1",
+    resultPerPage: "1000",
+  });
+
+  const productRoutes: MetadataRoute.Sitemap = products.documents.map(
+    ({ slug, $updatedAt }) => ({
+      url: `${config.next_app_base_url}/products/${slug}`,
+      lastModified: new Date($updatedAt),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    })
+  );
+
+  const blogRoutes: MetadataRoute.Sitemap = blogs.documents.map(
+    ({ slug, $updatedAt }) => ({
+      url: `${config.next_app_base_url}/blog/${slug}`,
+      lastModified: new Date($updatedAt),
+      changeFrequency: "monthly",
+      priority: 0.4,
+    })
+  );
+
   return [
     {
       url: `${config.next_app_base_url}`,
@@ -16,23 +50,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${config.next_app_base_url}/combo-deals`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
       url: `${config.next_app_base_url}/products`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
-    {
-      url: `${config.next_app_base_url}/offer`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-
+    ...productRoutes,
     {
       url: `${config.next_app_base_url}/blog`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.6,
     },
+    ...blogRoutes,
   ];
 }
--- src/components/Shared/ImageUploader/SelectFromLibrary/index.tsx
+++ src/components/Shared/ImageUploader/SelectFromLibrary/index.tsx
@@ -28,8 +28,6 @@ const SelectFromLibrary = ({
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
 
-  console.log({ images, total });
-
   const fetchImages = async () => {
     // handle loading
     if (!images?.length) {
