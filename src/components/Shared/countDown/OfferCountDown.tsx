c9bbaee0de2299329072f6e5897d65b43fbc87db
developer-loischloe <162805095+developer-loischloe@users.noreply.github.com>
2025-03-28 11:55:57 +0600

    Update OfferCountDown.tsx

--- src/components/Shared/countDown/OfferCountDown.tsx
+++ src/components/Shared/countDown/OfferCountDown.tsx
@@ -4,16 +4,16 @@ import CountDown from "./CountDown";
 
 const OfferCountDown = () => {
   const [timeInSideDhaka, setTimeInSideDhaka] = useState<null | Date>(
-    new Date("3/27/2025 20:00:00")
+    new Date("4/4/2025 20:00:00")
   );
   useEffect(() => {
-    setTimeInSideDhaka(new Date("3/27/2025 20:00:00"));
+    setTimeInSideDhaka(new Date("4/4/2025 20:00:00"));
   }, []);
 
   return (
     <section>
       <h3 className="text-2xl md:text-4xl font-bold text-center mb-5 uppercase">
-        LAST CHANCE FOR DELIVERY:
+         DELIVERY OPEN FROM:
       </h3>
       <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
         <div className="flex flex-col justify-center gap-1">
