d905932aaf961c35f36f7db7deefee78f11a2f80
shamim1845 <developer.loischloe@gmail.com>
2025-04-09 20:08:18 +0600

    offer banner modified

--- src/components/Offer/OfferBanner.tsx
+++ src/components/Offer/OfferBanner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import banner from "../../../public/home_slider/2000x757_px/Web_1.png";
+import banner from "../../../public/home_slider/2000x757_px/eid_bundle.jpg";
 
 const OfferBanner = () => {
   return (
