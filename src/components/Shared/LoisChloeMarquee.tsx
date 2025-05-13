e4027838d86549e8f258a5ce5ecd6e0263d0c047
developer-loischloe <developer.loischloe@gmail.com>
2025-05-13 14:58:35 +0600

    Marquee text changed

--- src/components/Shared/LoisChloeMarquee.tsx
+++ src/components/Shared/LoisChloeMarquee.tsx
@@ -7,18 +7,9 @@ const LoisChloeMarquee = () => {
       <div className="bg-[#FA9E00] hover:bg-black hover:text-white transition-all duration-300 p-2">
         <Marquee className="space-x-20">
           <div className="flex justify-between gap-x-40">
-            <div>
-              🎉✨ Eid Special Offer! ✨🎉 Enjoy 5% OFF on All Products –
-              Celebrate & Save! 🛍️💖
-            </div>
-            <div>
-              🎉✨ Eid Special Offer! ✨🎉 Enjoy 5% OFF on All Products –
-              Celebrate & Save! 🛍️💖
-            </div>
-            <div>
-              🎉✨ Eid Special Offer! ✨🎉 Enjoy 5% OFF on All Products –
-              Celebrate & Save! 🛍️💖
-            </div>
+            <div>🎉 Enjoy 5% OFF on All Products – Celebrate & Save! 🛍️💖</div>
+            <div>🎉 Enjoy 5% OFF on All Products – Celebrate & Save! 🛍️💖</div>
+            <div>🎉 Enjoy 5% OFF on All Products – Celebrate & Save! 🛍️💖</div>
           </div>
         </Marquee>
       </div>
